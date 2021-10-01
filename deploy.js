c2e1881a90edc6998e0a1434e915877e7e230a11
Martin <m>
2021-10-01 00:32:08 -0300

    Agregamos deploy y test. Fin modulo 2

--- deploy.js
+++ deploy.js
@@ -1,1 +1,27 @@
-// deploy code will go here
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+
+const provider = new HDWalletProvider(
+    "panel system popular occur insane rapid hammer tube dizzy duck judge owner",
+    "https://rinkeby.infura.io/v3/1b470ee288ea462f80f3f81e04927ce4"
+);
+
+const web3 = new Web3(provider);
+
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("deployando", accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Hi there!"] })
+        .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+    
+    
+    console.log("deployando", result.options.address);
+};
+
+deploy();
